e8f623973dc3c01671c13cfdf762191fd74e27e3
Michelinchen <michael.kecker@googlemail.com>
2026-05-20 16:54:15 +0200

    M3: CorsConfig hinzugefügt

---
+++ src/main/java/htw/webtech/NutriTrack/CorsConfig.java
@@ -0,0 +1,16 @@
+package htw.webtech.NutriTrack;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("http://localhost:5173", "https://nutritrack-frontend-q0wc.onrender.com");
+    }
+}
